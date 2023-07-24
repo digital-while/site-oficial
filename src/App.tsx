5a6f42662a8df883be0177e69ece5b366cd5c596
beatriz.mandim <beatriz.mandim@2mi.com.br>
2023-07-24 18:56:51 -0300

    feat: install react rounter dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Routes from "./routes";
+import { Routes, Route, useNavigate } from "react-router-dom";
 /* import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"; */
 
