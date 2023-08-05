902bf5819ac0919fa24ab3b73b49bd30a8b8429e
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-08-04 22:13:55 -0300

    Select Done

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,34 +1,48 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/userContext";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function SelectComponent() {
-    const [age, setAge] = React.useState('');
+  const { userInfo } = useContext(UserContext);
+  const navigate = useNavigate();
+  const { product } = useParams();
 
-    const handleChange = (event: SelectChangeEvent) => {
-        setAge(event.target.value as string);
-    };
+  const handleChange = (event: SelectChangeEvent) => {
+    console.log(event.target.value as string);
+    console.log(product);
+    navigate(`/${event.target.value}`);
+  };
 
-    return (
-        <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-            <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            label="Age"
-            onChange={handleChange}
-            >
-            <MenuItem value={10}>Social Media</MenuItem>
-            <MenuItem value={20}>Gestão de tráfego</MenuItem>
-            <MenuItem value={30}>Design</MenuItem>
-            <MenuItem value={30}>Sites</MenuItem>
-            </Select>
-        </FormControl>
-        </Box>
-    );
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value=""
+          label="Produto"
+          onChange={handleChange}
+        >
+          {userInfo.controlPanel.productEdit.map((productCategory, index) => {
+            if (product != productCategory.urlName) {
+              return (
+                <MenuItem
+                  key={index}
+                  value={`product/${productCategory.urlName}`}
+                >
+                  {productCategory.title}
+                </MenuItem>
+              );
+            }
+          })}
+        </Select>
+      </FormControl>
+    </Box>
+  );
 }
-
