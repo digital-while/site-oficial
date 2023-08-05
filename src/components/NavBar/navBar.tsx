ce56448308d8a6e1f8c2d236b3e1116fb16a2174
beatriz.mandim <beatriz.mandim@2mi.com.br>
2023-08-04 21:19:56 -0300

    test nav

--- src/components/NavBar/navBar.tsx
+++ src/components/NavBar/navBar.tsx
@@ -48,13 +48,16 @@ export default function NavBar(props: Props) {
         <List>
             <ul className="boxNavOptions-mobile">
                 <li className="container-nav">
-                    <Link className="option-nav" to="/menu">Menu</Link>
+                    <Link className="option-nav" to="/menu">Sobre nós</Link>
                 </li>
                 <li className="container-nav">
-                    <Link className="option-nav" to="/admin">Admin</Link>
+                    <Link className="option-nav" to="/admin">Serviços</Link>
                 </li>
                 <li className="container-nav">
-                    <Link className="option-nav" to="/">Sair</Link>
+                    <Link className="option-nav" to="/">Contatos</Link>
+                </li>
+                <li className="container-nav">
+                    <Link className="option-nav" to="/">Atualizações</Link>
                 </li>
             </ul>
         </List>
@@ -88,13 +91,16 @@ export default function NavBar(props: Props) {
             <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
                 <ul className="boxNavOptions">
                     <li className="container-desktop">
-                        <Link className="option-nav" to="/menu">Menu</Link>
+                        <Link className="option-nav" to="/menu">Sobre nós</Link>
                     </li>
                     <li className="container-desktop">
-                        <Link className="option-nav" to="/admin">Admin</Link>
+                        <Link className="option-nav" to="/admin">Serviços</Link>
                     </li>
                     <li className="container-desktop">
-                        <Link className="option-nav" to="/">Sair</Link>
+                        <Link className="option-nav" to="/">Contatos</Link>
+                    </li>
+                    <li className="container-desktop">
+                        <Link className="option-nav" to="/">Atualizações</Link>
                     </li>
                 </ul>
             </Box>
@@ -117,43 +123,6 @@ export default function NavBar(props: Props) {
             {drawer}
             </Drawer>
         </Box>
-        <Box component="main" sx={{ p: 3 }}>
-            <Toolbar />
-            <Typography>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde
-            fugit veniam eius, perspiciatis sunt? Corporis qui ducimus quibusdam,
-            aliquam dolore excepturi quae. Distinctio enim at eligendi perferendis in
-            cum quibusdam sed quae, accusantium et aperiam? Quod itaque exercitationem,
-            at ab sequi qui modi delectus quia corrupti alias distinctio nostrum.
-            Minima ex dolor modi inventore sapiente necessitatibus aliquam fuga et. Sed
-            numquam quibusdam at officia sapiente porro maxime corrupti perspiciatis
-            asperiores, exercitationem eius nostrum consequuntur iure aliquam itaque,
-            assumenda et! Quibusdam temporibus beatae doloremque voluptatum doloribus
-            soluta accusamus porro reprehenderit eos inventore facere, fugit, molestiae
-            ab officiis illo voluptates recusandae. Vel dolor nobis eius, ratione atque
-            soluta, aliquam fugit qui iste architecto perspiciatis. Nobis, voluptatem!
-            Cumque, eligendi unde aliquid minus quis sit debitis obcaecati error,
-            delectus quo eius exercitationem tempore. Delectus sapiente, provident
-            corporis dolorum quibusdam aut beatae repellendus est labore quisquam
-            praesentium repudiandae non vel laboriosam quo ab perferendis velit ipsa
-            deleniti modi! Ipsam, illo quod. Nesciunt commodi nihil corrupti cum non
-            fugiat praesentium doloremque architecto laborum aliquid. Quae, maxime
-            recusandae? Eveniet dolore molestiae dicta blanditiis est expedita eius
-            debitis cupiditate porro sed aspernatur quidem, repellat nihil quasi
-            praesentium quia eos, quibusdam provident. Incidunt tempore vel placeat
-            voluptate iure labore, repellendus beatae quia unde est aliquid dolor
-            molestias libero. Reiciendis similique exercitationem consequatur, nobis
-            placeat illo laudantium! Enim perferendis nulla soluta magni error,
-            provident repellat similique cupiditate ipsam, et tempore cumque quod! Qui,
-            iure suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
-            Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore commodi
-            reprehenderit rerum reiciendis! Quidem alias repudiandae eaque eveniet
-            cumque nihil aliquam in expedita, impedit quas ipsum nesciunt ipsa ullam
-            consequuntur dignissimos numquam at nisi porro a, quaerat rem repellendus.
-            Voluptates perspiciatis, in pariatur impedit, nam facilis libero dolorem
-            dolores sunt inventore perferendis, aut sapiente modi nesciunt.
-            </Typography>
-        </Box>
         </Box>
         
     );
@@ -161,29 +130,3 @@ export default function NavBar(props: Props) {
 
 
 
-
-
-/* export default function NavBar() {
-    const { userInfo } = useContext(UserContext)
-    return (
-        <>
-            <StyledHeader>
-                
-                    <img className="logo" src={userInfo.logo} alt="logo da empresa" />
-                    <ul className="boxNavOptions">
-                        <li>
-                            <Link to="/menu">Menu</Link>
-                        </li>
-                        <li>
-                            <Link to="/admin">Admin</Link>
-                        </li>
-                        <li>
-                            <Link to="/">Sair</Link>
-                        </li>
-                    </ul>
-                
-            </StyledHeader>
-        </>
-
-    )
-} */
