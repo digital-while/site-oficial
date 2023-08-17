c02fae889b9d9e11051763e7b137576242c885a5
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-08-17 20:08:08 -0300

    Merged with develop

--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -1,20 +1,34 @@
+import { useState } from "react";
 import { iProductCardProps } from "../../interfaces/components";
 import { StyledProductCard } from "./style";
 
 export const ProductCard = ({ product }: iProductCardProps) => {
+  const [view, SetView] = useState(false);
+
+  const handleClick = () => {
+    console.log("click");
+    SetView(!view);
+  };
+
   return (
-    <StyledProductCard blocked={product.blocked}>
-      <div className="card--front">
-        <div className="div--img">
-          <img src={product.img} alt="Uma imagem" />
+    <StyledProductCard
+      blocked={product.blocked}
+      onClick={handleClick}
+      backview={view}
+    >
+      <div className="card--inner">
+        <div className="card--front">
+          <div className="div--img">
+            <img src={product.img} alt="Uma imagem" />
+          </div>
+          <div className="div--text">
+            <h3>{product.name}</h3>
+            <p>{product.littleDescription}</p>
+          </div>
         </div>
-        <div className="div--text">
-          <h3>{product.name}</h3>
-          <p>{product.littleDescription}</p>
+        <div className="card--back">
+          <p>{product.largeDescription}</p>
         </div>
-      </div>
-      <div className="card--back">
-        <p>{product.largeDescription}</p>
       </div>
     </StyledProductCard>
   );
--- src/components/productCard/style.ts
+++ src/components/productCard/style.ts
@@ -2,45 +2,57 @@ import styled, { css } from "styled-components";
 import { iStyledProductCardProps } from "../../interfaces/components";
 
 export const StyledProductCard = styled.div<iStyledProductCardProps>`
-  border: 1px solid black;
-
-  ${
-    //Caso seja um produto bloqueado
-    ({ blocked }) =>
-      blocked &&
-      css`
-        border: 1px solid red;
-      `
-  }
-
   height: 50vh;
   width: 25%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
 
-  //ALTERAR HOVER APÓS BIBLIOTECA DE FLIPAR O CARD
-  &:hover {
-    .card--front {
-      display: none;
+  .card--inner {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    position: relative;
+    height: 100%;
+    width: 100%;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
+
+    border: 1px solid black;
+    ${
+      //Caso seja um produto bloqueado
+      ({ blocked }) =>
+        blocked &&
+        css`
+          border: 1px solid red;
+        `
     }
 
-    .card--back {
-      display: flex;
+    > div {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      backface-visibility: hidden;
     }
   }
 
-  > div {
-    height: 100%;
+  ${
+    //Animação para flipar o card
+    ({ backview }) =>
+      backview &&
+      css`
+        .card--inner {
+          transform: rotateY(180deg);
+        }
+      `
   }
 
   .card--front {
     .div--img {
       height: 50%;
-
+      width: 100%;
       img {
         /* ALTERAR PARA ENCAIXAR A IMAGEM */
         height: 100%;
+        width: 100%;
+        object-fit: cover;
       }
     }
 
@@ -51,7 +63,7 @@ export const StyledProductCard = styled.div<iStyledProductCardProps>`
   }
 
   .card--back {
-    display: none;
+    transform: rotateY(180deg);
     padding: 1rem;
     height: 100%;
     align-items: center;
